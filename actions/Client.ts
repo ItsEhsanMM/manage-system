6e309c79489458f82c710b667cb8ba05a0b13d19
ItsEhsanMM <itsehsanmm@gmail.com>
2025-02-19 17:57:41 +0330

    add server function to get clients statistics

--- actions/Client.ts
+++ actions/Client.ts
@@ -34,11 +34,74 @@ export const getClients = createServerAction().handler(async () => {
     const clients = await Client.find({
       managerID: new mongoose.Types.ObjectId(currentUser?.user?.id)
     })
-      .sort({ status: 1,joinDate:-1 }).exec()
+      .sort({ status: 1, joinDate: -1 })
+      .exec()
 
     return JSON.stringify(clients)
   } catch (error) {
     console.error(error)
     return []
+  }
+})
+
+export const clientStatistic = createServerAction().handler(async () => {
+  await connectDB()
+  const currentUser = await auth()
+
+  try {
+    const clients = await Client.find({
+      managerID: new mongoose.Types.ObjectId(currentUser?.user?.id)
+    })
+      .sort({ status: 1, joinDate: -1 })
+      .exec()
+
+    const clientCount = clients.length
+    let hired = 0
+    let fired = 0
+
+    clients.map(client => {
+      if (client.status === 'hired') {
+        hired += 1
+      } else {
+        fired += 1
+      }
+    })
+
+    return JSON.stringify([
+      {
+        id: 0,
+        content: 'کل کاربران',
+        count: clientCount
+      },
+      {
+        id: 1,
+        content: 'استخدامی ها',
+        count: hired
+      },
+      {
+        id: 2,
+        content: 'اخراجی ها',
+        count: fired
+      }
+    ])
+  } catch (error) {
+    console.error(error)
+    return [
+      {
+        id: 0,
+        content: 'کل کاربران',
+        count: 0
+      },
+      {
+        id: 1,
+        content: 'استخدامی ها',
+        count: 0
+      },
+      {
+        id: 2,
+        content: 'اخراجی ها',
+        count: 0
+      }
+    ]
   }
 })
